d547ce2719af9ad75b903a917c6129b3c05e04d3
Kristian Freeman <kristian@kristianfreeman.com>
2021-12-13 11:48:18 -0600

    Short URLs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="mx-auto max-w-xl flex flex-col justify-center items-center p-8">
-  <a href="https://twitter.com/signalnerve">
+  <a href="/twitter">
 	  <img
 	  	alt="Code with Kristian"
 	  	class="rounded-full w-24 transition-all hover:scale-110 hover:border-4 hover:border-orange-400 mb-4"
@@ -15,15 +15,15 @@
 	<h1 class="text-2xl mb-4 font-bold">Code with Kristian</h1>
 
 	<div class="flex mb-8 space-x-8">
-		<a class="hover:text-orange-400 transition-all" href="https://twitter.com/signalnerve">
+		<a class="hover:text-orange-400 transition-all" href="/twitter">
       {@html feather.icons.twitter.toSvg()}
     </a>
 
-		<a class="hover:text-orange-400 transition-all" href="https://github.com/signalnerve">
+		<a class="hover:text-orange-400 transition-all" href="/github">
       {@html feather.icons.github.toSvg()}
     </a>
 
-		<a class="hover:text-orange-400 transition-all" href="https://youtube.com/codewithkristian">
+		<a class="hover:text-orange-400 transition-all" href="/sub">
       {@html feather.icons.youtube.toSvg()}
     </a>
 	</div>
@@ -33,7 +33,7 @@
       icon="star"
       iconStyle="fill-current text-orange-400"
 			text="Weekly videos on programming, dev career, and more"
-			url="https://www.youtube.com/codewithkristian?sub_confirmation=1"
+			url="/sub"
 		/>
 
 		<Link
