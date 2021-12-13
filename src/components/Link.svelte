6ff2a8067d75d5359bbb34c8f69a0080392b80b6
Kristian Freeman <kristian@kristianfreeman.com>
2021-12-13 10:58:42 -0600

    Fix build

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,6 +1,6 @@
 <script>
   import feather from 'feather-icons'
-  export let icon, iconStyle, image, text, url;
+  export let icon, iconStyle, text, url;
 </script>
 
 <a class="hover:scale-105 transition-all shadow-md flex items-center p-4 px-6 rounded-full w-full" href={url}>
@@ -8,9 +8,6 @@
     {@html feather.icons[icon].toSvg({
       class: iconStyle,
     })}
-  {/if}
-  {#if image}
-  <img alt={text} class="w-14 h-14 object-cover" src={image}>
   {/if}
   <span class="ml-4">{text}</span>
 </a>
