06c4d11e8c20b1e1ab9d4fb37ed3961c3cb79674
Kristian Freeman <kristian@kristianfreeman.com>
2021-12-13 15:54:23 -0600

    Focus

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -3,7 +3,7 @@
   export let icon, iconStyle, text, url;
 </script>
 
-<a class="hover:scale-105 transition-all shadow-md flex items-center p-4 px-6 rounded-full w-full" href={url}>
+<a class="focus:scale-110 hover:scale-110 transition-all shadow-md flex items-center p-4 px-6 rounded-full w-full" href={url}>
   {#if icon}
     {@html feather.icons[icon].toSvg({
       class: iconStyle,
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <div class="mx-auto max-w-xl flex flex-col justify-center items-center p-8">
-  <a href="/twitter">
+  <a href="/twitter" tabindex="-1">
 	  <img
 	  	alt="Code with Kristian"
-	  	class="rounded-full w-24 transition-all hover:scale-110 hover:border-4 hover:border-orange-400 mb-4"
+	  	class="rounded-full w-24 transition-all hover:scale-125 hover:border-4 hover:border-orange-400 mb-4"
 	  	src="/kristian.png"
 	  />
   </a>
@@ -14,15 +14,15 @@
 	<h1 class="text-2xl mb-4 font-bold">Code with Kristian</h1>
 
 	<div class="flex mb-8 space-x-8">
-		<a class="hover:text-orange-400 transition-all" href="/twitter">
+		<a class="focus:scale-125 hover:scale-125 transition-all" href="/twitter">
 			<img alt="Twitter" class="w-8" src="/icons/twitter.gif" />
     </a>
 
-		<a class="hover:text-orange-400 transition-all" href="/sub">
+		<a class="focus:scale-125 hover:scale-125 transition-all" href="/sub">
 			<img alt="YouTube" class="w-8" src="/icons/youtube.gif" />
     </a>
 
-		<a class="hover:text-orange-400 transition-all" href="/github">
+		<a class="focus:scale-125 hover:scale-125 transition-all" href="/github">
 			<img alt="GitHub" class="w-8" src="/icons/github.gif" />
     </a>
 	</div>
